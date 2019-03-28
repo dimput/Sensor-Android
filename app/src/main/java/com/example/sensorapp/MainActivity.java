682b43aee14418bba22cebdd4b57ef9918b8e151
dimput <dimasputra@mail.ugm.ac.id>
2019-03-28 13:39:08 +0700

    Sensor Light, Magnet,Humidity,Acc,Temperature

--- app/src/main/java/com/example/sensorapp/MainActivity.java
+++ app/src/main/java/com/example/sensorapp/MainActivity.java
@@ -13,32 +13,48 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
     private SensorManager sensorManager;
-    private TextView textView;
-    private TextView sensorProx;
-    private Sensor sensorProximity;
+    private TextView sensorProx,sensorSuhu,sensorAcc,sensorHumidity,sensorLamp,sensorMagnet;
+    private Sensor sensorProximity,sensorTemperature,sensorAccelerometer,sensorKelembaban,sensorLampu,sensorMagneto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.sensol_list);
         sensorProx = (TextView) findViewById(R.id.sensol_prox);
+        sensorSuhu = (TextView) findViewById(R.id.sensor_suhu);
+        sensorLamp = (TextView) findViewById(R.id.sensor_lamp);
+        sensorMagnet = (TextView) findViewById(R.id.sensor_magnet);
+        sensorAcc = (TextView) findViewById(R.id.sensor_acc);
+        sensorHumidity = (TextView) findViewById(R.id.sensor_humidity);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
-
-        StringBuilder sensorText = new StringBuilder();
-        for(Sensor sensor:sensorList){
-            sensorText.append(sensor.getName())
-                    .append(System.getProperty("Line.Separator"));
-        }
-        textView.setText(sensorText);
 
         sensorProximity = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         if(sensorProximity==null){
             sensorProx.setText("No Sensor !");
         }
+        sensorTemperature = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if(sensorTemperature==null){
+            sensorSuhu.setText("No Sensor !");
+        }
+        sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensorAccelerometer==null){
+            sensorAcc.setText("No Sensor !");
+        }
+        sensorKelembaban = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        if(sensorKelembaban==null){
+            sensorHumidity.setText("No Sensor !");
+        }
+        sensorLampu = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if(sensorLampu==null){
+            sensorLamp.setText("No Sensor !");
+        }
+        sensorMagneto = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        if(sensorMagneto==null){
+            sensorMagnet.setText("No Sensor !");
+        }
+
     }
 
     @Override
@@ -47,6 +63,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if(sensorProximity!=null){
             sensorManager.registerListener((SensorEventListener) this,sensorProximity,SensorManager.SENSOR_DELAY_NORMAL);
         }
+        if(sensorTemperature!=null){
+            sensorManager.registerListener((SensorEventListener) this,sensorTemperature,SensorManager.SENSOR_DELAY_NORMAL);
+        }if(sensorAccelerometer!=null){
+            sensorManager.registerListener((SensorEventListener) this,sensorAccelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        }if(sensorKelembaban!=null){
+            sensorManager.registerListener((SensorEventListener) this,sensorKelembaban,SensorManager.SENSOR_DELAY_NORMAL);
+        }if(sensorLampu!=null){
+            sensorManager.registerListener((SensorEventListener) this,sensorLampu,SensorManager.SENSOR_DELAY_NORMAL);
+        }if(sensorMagneto!=null){
+            sensorManager.registerListener((SensorEventListener) this,sensorMagneto,SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+
     }
 
     @Override
@@ -61,6 +90,21 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float value = sensorEvent.values[0];
         if(sensorType==Sensor.TYPE_PROXIMITY){
             sensorProx.setText("Prox : "+value);
+        }
+        if(sensorType==Sensor.TYPE_ACCELEROMETER){
+            sensorAcc.setText("ACC : "+value);
+        }
+        if(sensorType==Sensor.TYPE_AMBIENT_TEMPERATURE){
+            sensorSuhu.setText("Suhu : "+value);
+        }
+        if(sensorType==Sensor.TYPE_RELATIVE_HUMIDITY){
+            sensorHumidity.setText("Kelembapan : "+value);
+        }
+        if(sensorType==Sensor.TYPE_LIGHT){
+            sensorLamp.setText("Light : "+value);
+        }
+        if(sensorType==Sensor.TYPE_MAGNETIC_FIELD){
+            sensorMagnet.setText("Magnetic : "+value);
         }
     }
 
